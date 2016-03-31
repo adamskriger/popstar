e08f3befd837e4589c290dc10ad9c6fc11f76fe7
github username <github email address>
2016-03-31 19:56:38 -0400

    Images now appear. Before images were broken links

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,6 +3,8 @@ class Photo < ActiveRecord::Base
                     :storage => :s3,
                     :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
 
+                    validates_attachment_content_type :download, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
   def s3_credentials
     {:bucket => "popstar", :aws_access_key_id => ENV["AWS_ACCESS_KEY_ID"], :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]}
   end
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -6,7 +6,7 @@ Rails.application.config.before_initialize do
 end
 
 
-Paperclip::Attachment.default_options[:s3_host_name] = 'us-standard.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3.amazonaws.com'
 
 Paperclip::Attachment.default_options[:s3_region] = 'us-east-1'
 
