beeca84cccaaefca8997a2e94bac34c70d453988
github username <github email address>
2016-04-23 15:48:03 -0400

    updated favicon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
 
-  
+
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # Thanks to Ben Walker at Dev walks for the Instagram template on which this #site's foundation is based : https://www.devwalks.com/.
+  
   protect_from_forgery with: :exception
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -28,7 +28,7 @@ class PhotosController < ApplicationController
 
     respond_to do |format|
       if @photo.save
-        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
+        format.html { redirect_to @photo, notice: 'Your image was successfully uploaded.' }
         format.json { render :show, status: :created, location: @photo }
       else
         format.html { render :new }
